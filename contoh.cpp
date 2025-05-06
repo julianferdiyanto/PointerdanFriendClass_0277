dc943a21356c17644c803fb22cd74c4b423e1230
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-05-06 10:57:26 +0700

    membuat class persegi panjang dengan akses modifier public

--- contoh.cpp
+++ contoh.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+
+class PersegiPanjang
+{
+public:
+    int panjang, lebar;
+
+public: 
+
+    void input()
+    {
+        cout << "Masukan panjang = ";
+        cin >> panjang;
+        cout << "Masukan lebar = ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran ol;
+};
